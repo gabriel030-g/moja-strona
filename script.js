4480006fbfdf9b0cbce3f039384747fe4236e7ec
goaglaol <Gabriel_Pietrucha@o2.pl>
2026-08-04 13:24:48 +0200

    Dodano animacje scroll

---
+++ script.js
@@ -0,0 +1,28 @@
+const sections = document.querySelectorAll(".hidden");
+
+
+const observer = new IntersectionObserver((entries)=>{
+
+
+entries.forEach(entry=>{
+
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+
+});
+
+
+});
+
+
+
+sections.forEach(section=>{
+
+observer.observe(section);
+
+});
