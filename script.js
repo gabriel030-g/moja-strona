939f8731c6e39378b4eb3ab63cadf2ba5d2b39b3
goaglaol <Gabriel_Pietrucha@o2.pl>
2026-08-04 13:35:04 +0200

    Dodano animowany tekst

--- script.js
+++ script.js
@@ -25,4 +25,78 @@ sections.forEach(section=>{
 
 observer.observe(section);
 
-});
+});const text = document.querySelector("#typing");
+
+
+const words = [
+    "Początkujący Frontend Developer 🚀",
+    "Tworzę strony internetowe 💻",
+    "Uczę się JavaScript ⚡",
+    "Buduję własne projekty 🔥"
+];
+
+
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+
+
+function typingEffect(){
+
+
+const currentWord = words[wordIndex];
+
+
+if(!deleting){
+
+    text.textContent =
+    currentWord.substring(0,charIndex++);
+
+    
+    if(charIndex > currentWord.length){
+
+        deleting = true;
+
+        setTimeout(typingEffect,1500);
+
+        return;
+
+    }
+
+
+}else{
+
+
+    text.textContent =
+    currentWord.substring(0,charIndex--);
+
+
+    if(charIndex === 0){
+
+        deleting = false;
+
+        wordIndex++;
+
+        if(wordIndex === words.length){
+
+            wordIndex = 0;
+
+        }
+
+    }
+
+
+}
+
+
+setTimeout(
+typingEffect,
+deleting ? 50 : 100
+);
+
+
+}
+
+
+typingEffect();
