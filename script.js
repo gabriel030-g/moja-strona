8682d8e1322efd1c71b64d0e14719ced294a4897
goaglaol <Gabriel_Pietrucha@o2.pl>
2026-08-04 18:33:27 +0200

    opis zmian

--- script.js
+++ script.js
@@ -143,3 +143,42 @@ themeButton.addEventListener("click", ()=>{
 
 
 });
+// =========================
+// ANIMACJA PROJEKTÓW
+// =========================
+
+
+const projects = document.querySelectorAll(".project-card");
+
+
+const projectObserver = new IntersectionObserver((entries)=>{
+
+
+    entries.forEach(entry=>{
+
+
+        if(entry.isIntersecting){
+
+
+            entry.target.classList.add("show");
+
+
+        }
+
+
+    });
+
+
+},{
+
+    threshold:0.2
+
+});
+
+
+
+projects.forEach(project=>{
+
+    projectObserver.observe(project);
+
+});
